5424c20ff7db70dfb65210bf9940f7e900ad02e4
Sarah Beth Tracy <sarahbtracy17@gmail.com>
2019-07-03 18:57:03 -0700

    admin changes

--- PhotoApp/admin.py
+++ PhotoApp/admin.py
@@ -5,4 +5,6 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'location', 'uploaded_at', 'description',)
