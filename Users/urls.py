2f0b94d27c13dac1b60af91f4242f6b8e8fd09d8
Sarah Beth Tracy <sarahbtracy17@gmail.com>
2019-07-22 19:45:39 -0700

    removed bad app

--- Users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import HomePageView
-
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', HomePageView.as_view(), name= 'home'),
-]
